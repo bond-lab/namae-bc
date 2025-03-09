44a32b77a2845d73d3c0ab0c20cb76970a4fbce1
Francis Bond <fcbond@gmail.com>
2025-03-10 00:00:38 +0100

    normalize heisei names

--- scripts/add-heisei.py
+++ scripts/add-heisei.py
@@ -1,6 +1,21 @@
 import os
 import sys
 import sqlite3
+import re
+
+
+mapping = [('―', 'ー'),
+       ('－', 'ー'),
+       ('-', 'ー'),
+       ('‐',  'ー'),
+       ('晴', '晴'),
+       ('昻', '昂'),
+       # ('﨣', ''),
+       # ('靖', ''),
+       # ('礼', ''),
+       ]
+
+bad = "Ｍ＆ｎ?Ｆ～ＤＷＴ※Ｐ＋…ＩＧ．ｒＶＫ？Ｅ￥Ｃｋｍ＊Ｎ-ＬＺＳＪ－/：ＢｊＲ；ＡＯｖ"
 
 def load_heisei_data(data_dir, db_path):
     conn = sqlite3.connect(db_path)
@@ -16,10 +31,12 @@ def load_heisei_data(data_dir, db_path):
                         parts = line.strip().split('\t')
                         if len(parts) == 3:
                             name, frequency = parts[1], int(parts[2])
+                            for (f, t) in mapping:
+                                name = name.replace(f, t)
                             if name != '※希望により削除':
                                 c.execute(""" 
                                 INSERT INTO namae (year, orth, pron, loc, gender, explanation, src)
-                                VALUES (?, ?, ?, ?, ?, ?, ?)""", (year, name, '', '', gender, '', 'hc'))          
+                                VALUES (?, ?, ?, ?, ?, ?, ?)""", (year, name, '', '', gender, '', 'hs'))          
     conn.commit()
     conn.close()
 
