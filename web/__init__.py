9d9a49c607b1b30a244b3825cb48e97177fae998
Francis Bond <bond@ieee.org>
2025-06-09 16:59:15 +0200

    added macros for tables

--- web/__init__.py
+++ web/__init__.py
@@ -1,12 +1,15 @@
 """Initialize Flask Application."""
 from flask import Flask, session
 
+from web.filters import format_cell
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, template_folder="templates")
     app.secret_key = "namae"
-    
+
+    app.template_filter('format_cell')(format_cell)
+
     with app.app_context():
         from . import routes
 
